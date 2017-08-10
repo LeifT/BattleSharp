6bf28c8b8dfa3f85dfbd3847d088f3348c6044b5
Leif <leif_hedly_92@hotmail.com>
2017-08-11 00:07:11 +0200

    Testing different methods to deserialize.

--- BattleSharp/BattleClient.cs
+++ BattleSharp/BattleClient.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using BattleSharp.Utilities;
 using BattleSharp.Wow;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BattleSharp
 {
@@ -18,19 +19,21 @@ namespace BattleSharp
             APIKey = apiKey;
         }
 
-        //internal T DeserializeData<T>(string url) {
+        //internal T DeserializeData<T>(string url)
+        //{
         //    var client = new HttpClient();
         //    JsonSerializer serializer = new JsonSerializer();
 
         //    using (var s = client.GetStreamAsync(url).Result)
         //    using (StreamReader reader = new StreamReader(s))
-        //    using (JsonTextReader jsonReader = new JsonTextReader(reader)) {
+        //    using (JsonTextReader jsonReader = new JsonTextReader(reader))
+        //    {
         //        return serializer.Deserialize<T>(jsonReader);
         //    }
         //}
 
         //internal void PopulateData<T>(string url, out T asd) {
-         
+
         //    var client = new HttpClient();
         //    JsonSerializer serializer = new JsonSerializer();
 
@@ -41,9 +44,85 @@ namespace BattleSharp
         //    }
         //}
 
-        public async Task <Auctions> GetAuctions(string realm) {
+        public IList<Auction> GetAuctionsNew(string realm) {
+            var client = new HttpClient();
+            var jsonstring = client.GetStringAsync($"http://auction-api-us.worldofwarcraft.com/auction-data/ab1239c3bc437d48321a64e6b5e5ab7f/auctions.json").GetAwaiter().GetResult();
+            
 
+            JObject googleSearch = JObject.Parse(jsonstring);
+
+            IList<JToken> results = googleSearch["auctions"].Children().ToList();
+            IList<Auction> searchResults = new List<Auction>();
+
+            foreach (JToken result in results) {
+                Auction searchResult = result.ToObject<Auction>();
+                searchResults.Add(searchResult);
+            }
+
+            return searchResults;
+        }
+
+        public async Task<IList<Auction>> GetAuctionsNewAsync(string realm) {
+            var client = new HttpClient();
+            var task = client.GetStreamAsync("http://auction-api-us.worldofwarcraft.com/auction-data/ab1239c3bc437d48321a64e6b5e5ab7f/auctions.json");
+
+            JObject googleSearch;
+   
+            using (StreamReader streamReader = new StreamReader(await task.ConfigureAwait(false))) {
+                using (JsonTextReader jsonReader = new JsonTextReader(streamReader)) {
+                    googleSearch = await JObject.LoadAsync(jsonReader);
+                }
+            }
+
+            var results = googleSearch["auctions"].Children();
+            IList<Auction> searchResults = new List<Auction>();
+
+
+            foreach (JToken result in results)
+            {
+                Auction searchResult = result.ToObject<Auction>();
+                searchResults.Add(searchResult);
+            }
+
+            //Console.WriteLine(searchResults);
+
+            return searchResults;
+        }
+
+        public Auctions GetAuctions2(string realm) {
+            var client = new HttpClient();
+            var jsonstring = client.GetStringAsync($"https://eu.api.battle.net/wow/auction/data/{realm}?locale=en_GB&apikey={APIKey}").GetAwaiter().GetResult();
+            var files = JsonConvert.DeserializeObject<AuctionFiles>(jsonstring);
+
+            HashSet<string> urlSet = new HashSet<string>();
+            List<string> urlList = new List<string>();
+
+            foreach (var auctionFile in files) {
+                if (urlSet.Add(auctionFile.Url)) {
+                    urlList.Add(auctionFile.Url);
+                }
+            }
+
+            return JsonConvert.DeserializeObject<Auctions>(client.GetStringAsync(urlList[0]).GetAwaiter().GetResult());
+        }
+
+        public Auctions GetAuctions(string realm) {
             var files = JsonUtilities.DeserializeStreamFromUrl<AuctionFiles>($"https://eu.api.battle.net/wow/auction/data/{realm}?locale=en_GB&apikey={APIKey}");
+
+            HashSet<string> urlSet = new HashSet<string>();
+            List<string> urlList = new List<string>();
+
+            foreach (var auctionFile in files) {
+                if (urlSet.Add(auctionFile.Url)) {
+                    urlList.Add(auctionFile.Url);
+                }
+            }
+
+            return JsonUtilities.DeserializeStreamFromUrl<Auctions>(urlList[0]);
+        }
+
+        public async Task <Auctions> GetAuctionsAsync(string realm) {
+            var files = JsonUtilities.DeserializeStreamFromUrlAync<AuctionFiles>($"https://eu.api.battle.net/wow/auction/data/{realm}?locale=en_GB&apikey={APIKey}");
             
             HashSet<string> urlSet = new HashSet<string>();
             List<string> urlList = new List<string>();
@@ -53,23 +132,8 @@ namespace BattleSharp
                     urlList.Add(auctionFile.Url); 
                 }
             }
-
-             var hello = JsonUtilities.DeserializeStreamFromUrl<Auctions>(urlList[0]);
-
-            foreach (var auction in await hello) {
-                Console.WriteLine(auction.item);
-            }
-
-            var auctions = JsonUtilities.DeserializeStreamFromUrl<Auctions>(urlList[0]);
-
-      
-            //for (int i = 1; i < auctions.Count(); i++) {
-            //    auctions (JsonUtilities.DeserializeStreamFromUrl<Auctions>(urlList[i]));
-
-            //}
             
-
-            return null;
+            return await JsonUtilities.DeserializeStreamFromUrlAync<Auctions>(urlList[0]);
         }
         
         //public async Task<T> DeserializeData2<T>(string url) {
--- BattleSharp/Utilities/JsonUtilities.cs
+++ BattleSharp/Utilities/JsonUtilities.cs
@@ -17,67 +17,38 @@ namespace BattleSharp.Utilities {
                 throw new ArgumentException("Stream must support reading", nameof(jsonStream));
             }
 
-            JsonSerializer serializer = new JsonSerializer();
-
             T deserializedObject;
 
             using (StreamReader streamReader = new StreamReader(jsonStream)) {
                 using (JsonTextReader jsonReader = new JsonTextReader(streamReader)) {
+                    JsonSerializer serializer = new JsonSerializer();
                     deserializedObject = serializer.Deserialize<T>(jsonReader);
                 }
             }
 
-               
-
             return deserializedObject;
         }
 
-        //public static void DeserializeStream<T>(Stream jsonStream, T ob) where T : class {
-        //    if (jsonStream == null) {
-        //        throw new ArgumentNullException(nameof(jsonStream));
-        //    }
+        public static T DeserializeStreamFromUrl<T>(string url) where T : class {
+            var client = new HttpClient();
+            var task = client.GetStreamAsync(url);
+            return DeserializeStream<T>(task.ConfigureAwait(false).GetAwaiter().GetResult());
+        }
 
-        //    if (jsonStream.CanRead == false) {
-        //        throw new ArgumentException("Stream must support reading", nameof(jsonStream));
-        //    }
-
-        //    JsonSerializer serializer = new JsonSerializer();
-
-        //    using (StreamReader streamReader = new StreamReader(jsonStream)) {
-        //        using (JsonTextReader jsonReader = new JsonTextReader(streamReader)) {
-        //            serializer.Populate(jsonReader, ob);
-        //        }
-        //    }
-        //}
-
-        public static async Task<T> DeserializeStreamFromUrl<T>(string url) where T : class {
+        public static async Task<T> DeserializeStreamFromUrlAync<T>(string url) where T : class {
             var client = new HttpClient();
             var task = client.GetStreamAsync(url);
             
-            //return DeserializeStream<T>(task.GetAwaiter().GetResult());
-            //var jsonStream = DeserializeStream<T>(task.GetAwaiter().GetResult());
+            T deserializedObject;
 
-            // Task<T> deserializedObject;
-
-            Task<JObject> deserializedObject;
-
-            T potato;
-
-            using (StreamReader streamReader = new StreamReader(await task))
-            {
+            using (StreamReader streamReader = new StreamReader(await task.ConfigureAwait(false))) {
                 using (JsonTextReader jsonReader = new JsonTextReader(streamReader)) {
-                    JsonSerializer asdf = new JsonSerializer();
-                    potato = asdf.Deserialize<T>(jsonReader);
-                     deserializedObject = JObject.LoadAsync(jsonReader);
-                    //deserializedObject = serializer.Deserialize<T>(jsonReader);
+                    JsonSerializer serializer = new JsonSerializer();
+                    deserializedObject = serializer.Deserialize<T>(jsonReader);
                 }
             }
 
-            return potato;
-
-            var asd = deserializedObject.Result.ToObject<T>();
-            
-            return asd;
+            return deserializedObject;
         }
     }
 }
