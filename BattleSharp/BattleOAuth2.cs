fc05ffa7c715956f8d01bffcff4c3175ae94203c
Leif <leif_hedly_92@hotmail.com>
2018-01-27 00:18:28 +0100

    Implemented OAuth2

---
+++ BattleSharp/BattleOAuth2.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using BattleSharp.Utilities;
+
+namespace BattleSharp
+{
+    public class BattleOAuth2 {
+        private string _client_id;
+        private string _client_secret;
+
+        public BattleOAuth2(string client_id, string client_secret) {
+            _client_id = client_id;
+            _client_secret = client_secret;
+        }
+
+        public async Task<RootObject> GetAccessToken() {
+            return await JsonUtilities.DeserializeUrlAync<RootObject>($"https://eu.battle.net/oauth/token?grant_type=client_credentials&client_id={_client_id}&client_secret={_client_secret}");
+        }
+    }
+
+    public class RootObject
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+    }
+}
