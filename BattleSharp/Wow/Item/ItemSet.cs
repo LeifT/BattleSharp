fc9ba19c233d1523223c848d6b5134b2a32d93c2
Leif <leif_hedly_92@hotmail.com>
2018-02-21 23:29:24 +0100

    Added the ItemSet class.

---
+++ BattleSharp/Wow/Item/ItemSet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BattleSharp.Wow.Item {
+    public class ItemSet {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<SetBonus> SetBonuses { get; set; }
+        public ICollection<int> Items { get; set; }
+    }
+}
---
+++ BattleSharp/Wow/Item/SetBonus.cs
@@ -0,0 +1,6 @@
+﻿namespace BattleSharp.Wow.Item {
+    public class SetBonus {
+        public string Description { get; set; }
+        public int Threshold { get; set; }
+    }
+}
