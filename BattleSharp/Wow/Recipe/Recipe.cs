4f451667d8a306b035e3283ca7c31ba5ff2f58a1
Leif <leif_hedly_92@hotmail.com>
2018-02-24 19:31:40 +0100

    Added recipe class.

---
+++ BattleSharp/Wow/Recipe/Recipe.cs
@@ -0,0 +1,8 @@
+﻿namespace BattleSharp.Wow.Recipe {
+    public class Recipe {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public string Icon { get; set; }
+    }
+}
